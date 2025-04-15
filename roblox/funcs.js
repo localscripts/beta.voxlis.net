3ffb2f68f3b0777897471cf3e93d08f4cd884970
u/Failed_cocacola <123872886+localscripts@users.noreply.github.com>
2025-04-15 04:58:58 -0700

    Update funcs.js

--- roblox/funcs.js
+++ roblox/funcs.js
@@ -408,7 +408,7 @@ const expData = [
     priceHref: "https://yap.com/",
     info: "## Oops! 🤭\n- Looks like we had not gathered the information yet on this Exploit! This could take some time to finish...\n\nIf you would like to help us out, visit https://github.com/localscripts/voxlis.NET/blob/main/README.md!",
     hide: false,
-    hasKeySystem: true,
+    hasKeySystem: false,
     free: true,
   },
   {
